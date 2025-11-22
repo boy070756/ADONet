74731f8ea134db8bb879d05d05020204f4436504
TinhNguyen <tinh070756@gmail.com>
2025-11-22 20:50:10 +0700

    added lesson1

---
+++ Lesson1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Lesson1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSinhVien = new System.Windows.Forms.DataGridView();
+            this.btnLoadConnected = new System.Windows.Forms.Button();
+            this.btnLoadDisconnected = new System.Windows.Forms.Button();
+            this.btnLoadDisconnected1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSinhVien
+            // 
+            this.dgvSinhVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSinhVien.Location = new System.Drawing.Point(13, 25);
+            this.dgvSinhVien.Name = "dgvSinhVien";
+            this.dgvSinhVien.RowHeadersWidth = 72;
+            this.dgvSinhVien.RowTemplate.Height = 31;
+            this.dgvSinhVien.Size = new System.Drawing.Size(1728, 648);
+            this.dgvSinhVien.TabIndex = 0;
+            // 
+            // btnLoadConnected
+            // 
+            this.btnLoadConnected.Location = new System.Drawing.Point(13, 738);
+            this.btnLoadConnected.Name = "btnLoadConnected";
+            this.btnLoadConnected.Size = new System.Drawing.Size(183, 76);
+            this.btnLoadConnected.TabIndex = 1;
+            this.btnLoadConnected.Text = "Load Connected";
+            this.btnLoadConnected.UseVisualStyleBackColor = true;
+            this.btnLoadConnected.Click += new System.EventHandler(this.btnLoadConnected_Click);
+            // 
+            // btnLoadDisconnected
+            // 
+            this.btnLoadDisconnected.Location = new System.Drawing.Point(231, 738);
+            this.btnLoadDisconnected.Name = "btnLoadDisconnected";
+            this.btnLoadDisconnected.Size = new System.Drawing.Size(228, 76);
+            this.btnLoadDisconnected.TabIndex = 2;
+            this.btnLoadDisconnected.Text = "Load Disconnected";
+            this.btnLoadDisconnected.UseVisualStyleBackColor = true;
+            this.btnLoadDisconnected.Click += new System.EventHandler(this.btnLoadDisconnected_Click);
+            // 
+            // btnLoadDisconnected1
+            // 
+            this.btnLoadDisconnected1.Location = new System.Drawing.Point(505, 738);
+            this.btnLoadDisconnected1.Name = "btnLoadDisconnected1";
+            this.btnLoadDisconnected1.Size = new System.Drawing.Size(228, 76);
+            this.btnLoadDisconnected1.TabIndex = 3;
+            this.btnLoadDisconnected1.Text = "Load Disconnected 1";
+            this.btnLoadDisconnected1.UseVisualStyleBackColor = true;
+            this.btnLoadDisconnected1.Click += new System.EventHandler(this.btnLoadDisconnected1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1753, 1134);
+            this.Controls.Add(this.btnLoadDisconnected1);
+            this.Controls.Add(this.btnLoadDisconnected);
+            this.Controls.Add(this.btnLoadConnected);
+            this.Controls.Add(this.dgvSinhVien);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhVien)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSinhVien;
+        private System.Windows.Forms.Button btnLoadConnected;
+        private System.Windows.Forms.Button btnLoadDisconnected;
+        private System.Windows.Forms.Button btnLoadDisconnected1;
+    }
+}
+
---
+++ Lesson1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lesson1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter adapter;
+        DataSet dataSet;
+
+        string connectionString = @"data source=tinhnvt;user id= sa;password=boy100890;initial catalog=QuanLySinhVien;
+                                     Persist Security Info=false;
+                                     Encrypt=true;TrustServerCertificate=true;
+                                     Connection Timeout=30";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadConnected_Click(object sender, EventArgs e)
+        {
+            var query = "Select * From SinhVien";
+
+            var sqlConnect = new SqlConnection(connectionString);
+            sqlConnect.Open();
+
+            var sqlCommand = new SqlCommand(query, sqlConnect);
+            var sqlReader = sqlCommand.ExecuteReader();
+
+            // Tạo cột cho DataGridView (nếu chưa có)
+            dgvSinhVien.Columns.Clear();
+            dgvSinhVien.Columns.Add("MaSV", "Mã Sinh Viên");
+            dgvSinhVien.Columns.Add("HoTen", "Họ và Tên");
+            dgvSinhVien.Columns.Add("NgaySinh", "Ngày Sinh");
+            dgvSinhVien.Columns.Add("GioiTinh", "Giới Tính");
+            dgvSinhVien.Columns.Add("Lop", "Lớp");
+            dgvSinhVien.Columns.Add("Email", "Email");
+
+            // Đọc từng dòng và thêm vào DataGridView
+            while (sqlReader.Read())
+            {
+                dgvSinhVien.Rows.Add(sqlReader["MaSV"],
+                    sqlReader["HoTen"],
+                    sqlReader["NgaySinh"],
+                    sqlReader["GioiTinh"],
+                    sqlReader["NgaySinh"],
+                    sqlReader["Lop"],
+                    sqlReader["Email"]);
+            }
+
+            sqlReader.Close();
+            sqlConnect.Close();
+        }
+
+        public void LoadDisconnected()
+        {
+            var query = "Select * From SinhVien";
+
+            conn = new SqlConnection(connectionString);
+
+            adapter = new SqlDataAdapter(query, conn);
+            dataSet = new DataSet();
+            adapter.Fill(dataSet, "SinhVien");// Lấy dữ liệu vào DataSet và ngắt kết nối
+
+            dgvSinhVien.DataSource = dataSet.Tables["SinhVien"];
+
+            dgvSinhVien.Columns["MaSV"].HeaderText = "Mã Sinh Viên";
+            dgvSinhVien.Columns["HoTen"].HeaderText = "Họ và Tên";
+            dgvSinhVien.Columns["NgaySinh"].HeaderText = "Ngày Sinh";
+            dgvSinhVien.Columns["GioiTinh"].HeaderText = "Giới Tính";
+            dgvSinhVien.Columns["Lop"].HeaderText = "Lớp";
+            dgvSinhVien.Columns["Email"].HeaderText = "Email";
+        }
+
+        public void LoadDisconnected1()
+        {
+            var query = "Select * From SinhVien";
+
+            conn = new SqlConnection(connectionString);
+
+            adapter = new SqlDataAdapter(query, conn);
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);// Lấy dữ liệu vào DataTable và ngắt kết nối
+
+            dgvSinhVien.DataSource = table;
+        }
+
+        public void LoadDisconnected2()
+        {
+            var list = dataSet.Tables["SinhVien"].AsEnumerable()
+                 .Select(x => new
+                 {
+                     MaSV = x.Field<int>("MaSV"),
+                     HoTen = x.Field<string>("HoTen"),
+                     NgaySinh = x.Field<DateTime>("NgaySinh") 
+                 }).Take(2).ToList();
+
+            dgvSinhVien.DataSource = list;
+
+            dgvSinhVien.Columns["MaSV"].HeaderText = "Mã Sinh Viên";
+            dgvSinhVien.Columns["HoTen"].HeaderText = "Họ và Tên";
+            dgvSinhVien.Columns["NgaySinh"].HeaderText = "Ngày Sinh";
+        }
+
+        private void btnLoadDisconnected_Click(object sender, EventArgs e)
+        {
+            LoadDisconnected();
+        }
+
+        private void btnLoadDisconnected1_Click(object sender, EventArgs e)
+        {
+            LoadDisconnected2();
+        }
+    }
+}
